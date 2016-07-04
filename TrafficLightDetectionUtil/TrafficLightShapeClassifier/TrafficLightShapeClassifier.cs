0273bb79eb2802b33733e3c0af4b9a1dc2473ae9
stephendj <stephendj181194@gmail.com>
2016-07-04 20:17:13 +0700

    Added HOG traffic light shape classifier

--- TrafficLightDetectionUtil/TrafficLightShapeClassifier/TrafficLightShapeClassifier.cs
+++ TrafficLightDetectionUtil/TrafficLightShapeClassifier/TrafficLightShapeClassifier.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace TrafficLightDetectionUtil.TrafficLightShapeClassifier
 {
     public interface TrafficLightShapeClassifier
     {
+        TrafficLightRecognitionResult[] Classify(
+            Image<Bgr, byte> frame,
+            TrafficLightSegmentationResult[] trafficLightSegmentationResult);
     }
 }
---
+++ TrafficLightDetectionUtil/TrafficLightShapeClassifier/TrafficLightShapeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLightDetectionUtil.TrafficLightShapeClassifier
+{
+    public enum TrafficLightShapeType
+    {
+        Circle,
+        Left_Arrow,
+        Right_Arrow,
+        Unknown
+    }
+}
