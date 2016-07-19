3908b4a941491a67d0823cb62625828a39f46665
stephendj <stephendj181194@gmail.com>
2016-07-19 13:13:21 +0700

    Added GUI for traffic light detection

--- TrafficLightDetection/MainView.Designer.cs
+++ TrafficLightDetection/MainView.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace TrafficLightDetection
+﻿using System;
+
+namespace TrafficLightDetection
 {
     partial class MainView
     {
@@ -28,12 +30,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Video_Image_1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Video_Image_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Video_Image_1
+            // 
+            this.Video_Image_1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Video_Image_1.Location = new System.Drawing.Point(15, 25);
+            this.Video_Image_1.Name = "Video_Image_1";
+            this.Video_Image_1.Size = new System.Drawing.Size(570, 420);
+            this.Video_Image_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Video_Image_1.TabIndex = 1;
+            this.Video_Image_1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(15, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Browse Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.browse_image_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(495, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Browse Video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.browse_video_Click);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Traffic Light Detection";
+            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Video_Image_1);
+            this.Name = "MainView";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Video_Image_1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox Video_Image_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- TrafficLightDetection/MainView.cs
+++ TrafficLightDetection/MainView.cs
@@ -6,15 +6,81 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace TrafficLightDetection
 {
     public partial class MainView : Form
     {
+        private Capture cap = null;
+        private Timer timer = new Timer();
+        private int FPS = 40;
+
         public MainView()
         {
             InitializeComponent();
+
+            timer.Interval = 1000 / FPS;
+            timer.Tick += ProcessVideo;
+        }
+
+        // Browse Button
+        private void browse_image_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            FD.Title = "Choose Image";
+            //FD.InitialDirectory = @"C:\Users\Stephen Djohan\Documents\Visual Studio 2015\Projects\TrafficLightRecognition\TrafficLightRecognition\video";
+            FD.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            FD.FilterIndex = 2;
+            FD.RestoreDirectory = true;
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                ProcessImage(new Image<Bgr, byte>(FD.FileName));
+            }
+        }
+
+        private void browse_video_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            FD.Title = "Choose Image";
+            //FD.InitialDirectory = @"C:\Users\Stephen Djohan\Documents\Visual Studio 2015\Projects\TrafficLightRecognition\TrafficLightRecognition\video";
+            FD.Filter = "All Videos Files |*.dat; *.wmv; *.3g2; *.3gp; *.3gp2; *.3gpp; *.amv; *.asf;  *.avi; *.bin; *.cue; *.divx; *.dv; *.flv; *.gxf; *.iso; *.m1v; *.m2v; *.m2t; *.m2ts; *.m4v; " +
+                        " *.mkv; *.mov; *.mp2; *.mp2v; *.mp4; *.mp4v; *.mpa; *.mpe; *.mpeg; *.mpeg1; *.mpeg2; *.mpeg4; *.mpg; *.mpv2; *.mts; *.nsv; *.nuv; *.ogg; *.ogm; *.ogv; *.ogx; *.ps; *.rec; *.rm; *.rmvb; *.tod; *.ts; *.tts; *.vob; *.vro; *.webm";
+            FD.FilterIndex = 2;
+            FD.RestoreDirectory = true;
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                try { cap = new Capture(FD.FileName); }
+                catch (NullReferenceException ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                    return;
+                }
+                
+                timer.Start();
+            }
+        }
+
+        private void ProcessVideo(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> imgOri = cap.QueryFrame().ToImage<Bgr, Byte>();
+
+            if (imgOri != null)
+            {
+                Video_Image_1.Image = imgOri.Bitmap;
+            }
+            else
+            {
+                timer.Stop();
+            }            
+        }
+
+        private void ProcessImage(Image<Bgr, byte> imgOri)
+        {
+            Video_Image_1.Image = imgOri.Bitmap;
         }
     }
 }
